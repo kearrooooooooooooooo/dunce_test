68c5f6a299366f8ffcbe81898689df7f7ba40c80
kearrooooooooooooooo <32694622+kearrooooooooooooooo@users.noreply.github.com>
2018-07-01 19:34:40 -0700

    ecks dee

--- bigbrain.py
+++ bigbrain.py
@@ -1,7 +1,9 @@
-answer = int(input("What's 9+10?"))
-print(answer)
-if answer == 19:
-    print("You big brain")
-else:
-    print("You stoopid")
-      
+def bigbrain():
+    answer = int(input("What's 9+10? "))
+    print(answer)
+    if answer == 19:
+        print("You big brain")
+    else:
+        print("You stoopid")
+
+bigbrain()
