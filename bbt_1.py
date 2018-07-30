427e28a303387a690eff9867dadd4006f79afa3c
kearrooooooooooooooo <32694622+kearrooooooooooooooo@users.noreply.github.com>
2018-07-29 21:00:59 -0700

    fixed some shit
    still didn't do shit

--- bbt_1.py
+++ bbt_1.py
@@ -7,5 +7,10 @@ drink_base = ["slush", "green tea", "black tea", "red tea"]
 topping = ["pudding", "pearls", "grass jelly", "mango jelly", "lychee jelly"]
 
 #ugh can we do smth better than web scraping lmfao
-print("Welcome to the random bubble tea drink generator! Press Y to continue.")
+my_input = input("Welcome to the random bubble tea drink generator! Press y to continue.")
+print(my_input)
+if my_input == y:
+	print("Choose a flavor")
+else:
+	break
 
