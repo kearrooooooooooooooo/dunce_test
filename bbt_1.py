0073330c29c0d4237bbb6fb2d789aeece0946303
Anna Tang <aytang@sfu.ca>
2018-07-29 18:42:57 -0700

    WIP

---
+++ bbt_1.py
@@ -0,0 +1,10 @@
+import random
+
+flavors = ["taro", "strawberry", "chocolate", "original"]
+
+drink_base = ["slush", "green tea", "black tea"]
+
+topping = ["pudding", "pearls", "grass jelly", "mango jelly", "lychee jelly"]
+
+print("Welcome to the random bubble tea drink generator! Press Y to continue.")
+
