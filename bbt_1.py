c0e2007eef93ce2879099d91e0ca0d0a0b3433f0
kearrooooooooooooooo <32694622+kearrooooooooooooooo@users.noreply.github.com>
2018-07-29 20:50:48 -0700

    extra shit i did
    not much better than what u did rofl

--- bbt_1.py
+++ bbt_1.py
@@ -1,10 +1,11 @@
 import random
 
-flavors = ["taro", "strawberry", "chocolate", "original"]
+flavors = ["taro", "strawberry", "chocolate", "original", "mango", "peach", "watermelon", "green apple", "honeydew", "banana"]
 
-drink_base = ["slush", "green tea", "black tea"]
+drink_base = ["slush", "green tea", "black tea", "red tea"]
 
 topping = ["pudding", "pearls", "grass jelly", "mango jelly", "lychee jelly"]
 
+#ugh can we do smth better than web scraping lmfao
 print("Welcome to the random bubble tea drink generator! Press Y to continue.")
 
